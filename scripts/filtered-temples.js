daab604f88e003d716fc1615e96b8c06c2680b24
kehnniey <kehnniey@gmail.com>
2024-12-01 00:42:31 -0500

    aba

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -7,7 +7,7 @@ const temples = [
 	  location: "Aba, Nigeria",
 	  dedicated: "2005, August, 7",
 	  area: 11500,
-	  imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-248708-high-res-print.jpg"
+	  imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
 	},
 	{
 	  templeName: "Manti Utah",
